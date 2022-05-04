3e23519662bba4e3492c210c226112bfe20fde64
Saniya Baghirova <saniya.ibraimova@gmail.com>
2022-05-04 10:52:35 +0200

    Basic app with news overview

---
+++ client/index.jsx
@@ -0,0 +1,94 @@
+import React, { useEffect, useState } from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+
+async function fetchJSON(url) {
+  const res = await fetch(url);
+  if (!res.ok) {
+    throw new Error(`Failed to load ${res.status}: ${res.statusText}`);
+  }
+
+  return await res.json();
+}
+
+function NewsOverview() {
+  const { loading, error, data } = useLoading(async () => fetchJSON("/api/"));
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return (
+      <div>
+        <h1>Error</h1>
+        <div>{error.toString()}</div>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <h1>News overview</h1>
+      <ul>
+        {data.map((n) => (
+          <li key={n.title}>{n.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function useLoading(loadingFunction) {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState();
+  const [data, setData] = useState();
+
+  async function load() {
+    try {
+      setLoading(true);
+      setData(await loadingFunction());
+    } catch (error) {
+      setError(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    load();
+  }, []);
+
+  return { loading, error, data };
+}
+
+function FrontPage() {
+  return (
+    <div>
+      <div>
+        <h1>Daily News</h1>
+        <ul>
+          <li>
+            <Link to={"/login"}>Login</Link>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <NewsOverview />
+      </div>
+    </div>
+  );
+}
+
+function Application() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={"/"} element={<FrontPage />} />
+        <Route path={"/login"} element={<h1>Login</h1>} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+ReactDOM.render(<Application />, document.getElementById("app"));
--- server/server.js
+++ server/server.js
@@ -1,6 +1,30 @@
-import express from "express";
+import express, { Router } from "express";
+import * as path from "path";
 
 const app = express();
+const Login = new Router();
+const News = new Router();
+
+app.use(News);
+app.use(Login);
+
+News.get("/api/", (req, res) => {
+  res.json([
+    {
+      title: "Article 1",
+    },
+  ]);
+});
+
+app.use(express.static("../client/dist"));
+
+app.use((req, res, next) => {
+  if (req.method === "GET" && !req.path.startsWith("/api")) {
+    return res.sendFile(path.resolve("../client/dist/index.html"));
+  } else {
+    next();
+  }
+});
 
 const server = app.listen(process.env.PORT || 3000, () => {
   console.log(`Started on http://localhost:${server.address().port}`);
