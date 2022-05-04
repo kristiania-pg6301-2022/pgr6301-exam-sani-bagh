e5afd61df9e94d2b7959580ce489e02cec0e899c
Saniya Baghirova <saniya.ibraimova@gmail.com>
2022-05-04 23:40:03 +0200

    listArticles.test.jsx 🟢🟢🟢

--- client/pages/frontPage.jsx
+++ client/pages/frontPage.jsx
@@ -2,17 +2,17 @@ import { Link } from "react-router-dom";
 import React from "react";
 
 export function FrontPage() {
-    return (
-        <div>
-            <h1>News Database</h1>
-            <ul>
-                <li>
-                    <Link to={"/news"}>List news</Link>
-                </li>
-                <li>
-                    <Link to={"/news/new"}>Add new article</Link>
-                </li>
-            </ul>
-        </div>
-    );
+  return (
+    <div>
+      <h1>News Database</h1>
+      <ul>
+        <li>
+          <Link to={"/news"}>List news</Link>
+        </li>
+        <li>
+          <Link to={"/login"}>Add new article</Link>
+        </li>
+      </ul>
+    </div>
+  );
 }
