2fd012115b09143b2dcd4b9e91079d9d3e364d5d
Saniya Baghirova <saniya.ibraimova@gmail.com>
2022-05-05 11:41:22 +0200

    newsApi.test.js 🟢🟢🟢

---
+++ server/__tests__/newsApi.test.js
@@ -0,0 +1,45 @@
+import request from "supertest";
+import express from "express";
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import { NewsApi } from "../newsApi";
+
+dotenv.config();
+const app = express();
+app.use(bodyParser.json());
+
+const mongoClient = new MongoClient(process.env.MONGODB_URL);
+beforeAll(async () => {
+  await mongoClient.connect();
+  const database = mongoClient.db("test-db");
+  await database.collection("test-news").deleteMany({});
+  app.use("/api/news", NewsApi(database));
+});
+afterAll(() => {
+  mongoClient.close();
+});
+
+describe("news api", () => {
+  it("should add new article", async function () {
+    const author = "Test Author";
+    const title = "Test Title";
+    await request(app)
+      .post("/api/news")
+      .send({ title, author, topic: "Test Topic" })
+      .expect(200);
+    expect(
+      (
+        await request(app).get("/api/news").query({ author }).expect(200)
+      ).body.map(({ title }) => title)
+    ).toContain(title);
+  });
+
+  it("should list existing articles", async function () {
+    expect(
+      (await request(app).get("/api/news").expect(200)).body.map(
+        ({ title }) => title
+      )
+    ).toContain("Test Title");
+  });
+});
