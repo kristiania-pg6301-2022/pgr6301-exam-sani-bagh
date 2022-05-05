8ff7bd329ffd18dffc2594e9576b39df6a40d0d4
Saniya Baghirova <saniya.ibraimova@gmail.com>
2022-05-05 22:47:17 +0200

    fixed profile

--- client/pages/profile.jsx
+++ client/pages/profile.jsx
@@ -4,7 +4,15 @@ export function Profile({ user }) {
   return (
     <div>
       <h1>Profile</h1>
-      <pre>{JSON.stringify(user, undefined, "  ")}</pre>
+      <div className="profile">
+        <div className="box">
+          <h3>Name: {user.google.name}</h3>
+          <h4>Email: {user.google.email}</h4>
+        </div>
+        <div className="box">
+          <img src={user.google.picture} alt="Profile picture" />
+        </div>
+      </div>
     </div>
   );
 }
